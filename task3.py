6e9955ba9216c625c8045c13308665ced572e5cb
bonhamer <bonh248@deltalearns.ca>
2021-10-29 14:22:34 -0700

    1

--- task3.py
+++ task3.py
@@ -5,12 +5,15 @@ Create a function called title that creates a single line of output to display t
 "Title" with a box around it.  
 The function will have 1 optional input:
 symbol : string.  The symbolt to use as the border. Default symbol is "="
-
 The return value is the output string with escaped line breaks.
-
 example assertion:
 assert title("*") == "*********\n* Title *\n*********"
 assert title() == "=========\n= Title =\n========="
 (2 points)
-
 """
+
+def title(x = "="):
+    x = str(x)
+    return f"{x*9}\n{x} Title {x}\n{x*9}"
+
+print(title("="))
